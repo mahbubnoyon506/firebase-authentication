9bd4d55dd383f39a4d2af7bc34e2a4d48a4dac53
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-03-09 01:30:55 +0600

    login feature added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import auth from '../firebase.init';
 
 type FormValue = {
     email: string,
     password: string
 }
 
+
+
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
     const { register, formState: { errors }, handleSubmit } = useForm<FormValue>();
-    const onSubmit = (data: FormValue) => console.log(data);
+    const onSubmit = async (data: FormValue) => {
+        await signInWithEmailAndPassword(data.email, data.password)
+    };
+    if (user) {
+           console.log('user created')
+      }
     return (
         <div className='my-2 md:my-4 lg:my-6'>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='mb-2'>
                     <input
-                    className='bg-transparent text-lg p-2 border-b border-slate-50 focus:outline-none text-slate-50 w-full'
+                        className='bg-transparent text-lg p-2 border-b border-slate-50 focus:outline-none text-slate-50 w-full'
                         type='email'
                         placeholder='Email'
                         {...register("email", { required: "Email Address is required" })}
@@ -23,7 +38,7 @@ const Login = () => {
                 </div>
                 <div className='mb-2'>
                     <input
-                    className='bg-transparent text-lg p-2 border-b border-slate-50 border-0 focus:outline-none text-slate-50 w-full'
+                        className='bg-transparent text-lg p-2 border-b border-slate-50 border-0 focus:outline-none text-slate-50 w-full'
                         type='password'
                         placeholder='Password'
                         {...register("password", { required: "Password is required" })}
